8169e094e0d67e3aec54c6bff00ad7524d96f9f4
suuuuuuli <kozakbks2@gmail.com>
2026-05-18 17:24:53 +0200

    elo

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """
-ZTM Proxy — próbuje oba endpointy
+ZTM Proxy — próbuje oba endpointysss, zwraca dane w ujednoliconej formie. Używa tokena z env var ZTM_TOKEN.
 """
 from flask import Flask, jsonify, request
 import requests
